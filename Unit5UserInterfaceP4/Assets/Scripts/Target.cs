1a34704aacf349d555dc268796480aefb07295f9
irvinnet <irvingomez0331@gmail.com>
2023-05-01 11:41:10 -0700

    UI update
    added UI
    
    know bug is that score isnt being updated

--- Unit5UserInterfaceP4/Assets/Scripts/Target.cs
+++ Unit5UserInterfaceP4/Assets/Scripts/Target.cs
@@ -3,30 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Target : MonoBehaviour
-private Rigidbody targetRb;
-private float minSpeed = 12;
-private float maxSpeed = 16;
-private float maxTorque = 10;
-private float xRange = 4;
-private float ySpawnPos = -6;
 
 {
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+  
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
-        targetRb.AddTorque(RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+        transform.position = RandomSpwanPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(5);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     Vector3 RandomForce()
@@ -41,6 +55,6 @@ private float ySpawnPos = -6;
 
     Vector3 RandomSpwanPos()
     {
-        return new Vector3(Random.Ranger(-xRange, xRange), ySpawnPos);
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
